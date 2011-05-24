858622afad2b8120137f62547830dfca46557f5f
James Tucker <jftucker@gmail.com>
2011-05-23 19:07:54 -0700

    Add rake task for pre rails 3.1

--- lib/openid_active_record_store.rb
+++ lib/openid_active_record_store.rb
@@ -5,4 +5,24 @@ module OpenidActiveRecordStore
   class Engine < Rails::Engine
     config.eager_load_paths << File.expand_path("../../app/models", __FILE__)
   end
+  class Railtie < Rails::Railtie
+    rake_tasks do
+      namespace :openid_active_record_store do
+        namespace :install do
+          desc "install migrations"
+          task :migrations => %w[db/migrations/create_openid_associations.rb db/migrations/create_openid_nonces.rb]
+
+          cp_task = lambda do |t|
+            basename = File.basename(t.name)
+            cp File.expand_path("../../db/migrations/#{basename}", __FILE__), t.name
+          end
+
+          file 'db/migrations/create_openid_associations.rb', &cp_task
+
+          file 'db/migrations/create_openid_nonces.rb', &cp_task
+
+        end
+      end
+    end unless Gem::Version.new(Rails.version) >= Gem::Version.new('3.1.0')
+  end
 end
